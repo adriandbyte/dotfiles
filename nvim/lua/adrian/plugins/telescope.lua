f7c7817c2ae33f69aac3edcfe55baf0d1df2be8f
adrianbyte <adrian.dominguez.gomezz@gmail.com>
2025-11-01 00:58:06 -0600

    in the finder only search for files in the current directory

--- nvim/lua/adrian/plugins/telescope.lua
+++ nvim/lua/adrian/plugins/telescope.lua
@@ -42,10 +42,16 @@ return {
     local keymap = vim.keymap -- for conciseness
 
     keymap.set("n", "<leader>f", "/", { desc = "Find in current buffer" })
-    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+    keymap.set("n", "<leader>ff", function()
+      require('telescope.builtin').find_files({ cwd = vim.fn.expand('%:p:h') })
+    end, { desc = "Fuzzy find files in current directory" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+    keymap.set("n", "<leader>fs", function()
+      require('telescope.builtin').live_grep({ cwd = vim.fn.expand('%:p:h') })
+    end, { desc = "Find string in current directory" })
+    keymap.set("n", "<leader>fc", function()
+      require('telescope.builtin').grep_string({ cwd = vim.fn.expand('%:p:h') })
+    end, { desc = "Find string under cursor in current directory" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
     keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "Find todos" })
   end,
